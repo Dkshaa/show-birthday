3a5931d14075ccd82d1695c0ca6e9f656160df44
Dkshaa <diksha98620@gmail.com>
2026-07-02 15:23:00 -0700

    Show birthdays in cards

--- src/Data/Todo.js
+++ src/Data/Todo.js
@@ -1,26 +1,44 @@
-import { useState } from 'react'
-import { Data } from './data'
-import '../../src/index.css'
-const Todo=()=>{
-    const [todo,setTodo] = useState(Data)
-    const deleteHandler=(id)=>{
-        setTodo((oldPerson)=>{
-            let newPeople=oldPerson.filter((person)=>person.id!==id)
-            return newPeople
+import { useState } from 'react';
+import { Data } from './data';
+import '../../src/index.css';
+
+const Todo = () => {
+  const [people, setPeople] = useState(Data);
+
+  const removePerson = (id) => {
+    setPeople((currentPeople) =>
+      currentPeople.filter((person) => person.id !== id)
+    );
+  };
+
+  return (
+    <main className="birthday-card">
+      <h1>{people.length} birthdays today</h1>
+      {people.length === 0 ? (
+        <p className="empty-message">No birthdays left for today.</p>
+      ) : (
+        people.map((person) => {
+          const { id, name, age, image } = person;
+
+          return (
+            <article className="person" key={id}>
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years old</p>
+              </div>
+              <button className="text-btn" onClick={() => removePerson(id)}>
+                remove
+              </button>
+            </article>
+          );
         })
-    }
-    return(
-        <>
-        {todo.map((person)=>{
-            return(
-                <div className='item' key={person.id}>
-                    <h4>{person.name}</h4>
-                    <button className='btn' onClick={()=>deleteHandler(person.id)}>Clear</button>
-                </div>
-            )
-        })}
-        <button className='btn'onClick={()=>setTodo([])}>Clear All</button>
-        </>
-    )
-}
-export default Todo
+      )}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear All
+      </button>
+    </main>
+  );
+};
+
+export default Todo;
